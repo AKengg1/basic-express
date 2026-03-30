5cd7a00a4782c9a7f8906fb353457ad4dc9a29d8
AKengg1 <ak72532961@gmail.com>
2026-03-30 13:35:57 +0530

    middlewares add

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const express = require("express");
 
 const app = express();
 
+//middlewares
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()} - ${req.method} - ${req.url}]`);
+  next();
+});
+
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    console.log(`responded in ${Date.now() - start}ms`);
+  });
+  next();
+});
+//handmade database
+
+let posts = [
+  { id: 1, title: "Hello World", author: "alice", body: "My first post" },
+  { id: 2, title: "Express is great", author: "bob", body: "Learning backend" },
+  {
+    id: 3,
+    title: "Middleware magic",
+    author: "alice",
+    body: "Next and next and next",
+  },
+];
+let nextId = 4;
+
+//routes
 app.get("/", (_, res) => {
   res.send("Welcome to server");
 });
